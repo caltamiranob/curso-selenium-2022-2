f47dcd6b82c5589e9cf441c8f8509612068d645a
Carlos Altamirano B <alt.carlos@gmail.com>
2022-06-11 16:49:17 -0500

    Se agrega el driver opera en el metodo WebDriver

--- visorweb-junit-pom/src/main/java/pe/visorweb/driver/VisorWebDriver.java
+++ visorweb-junit-pom/src/main/java/pe/visorweb/driver/VisorWebDriver.java
@@ -3,18 +3,28 @@ package pe.visorweb.driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class VisorWebDriver {
 
-	public enum NavegadorWeb { FIREFOX, CHROME }
+	public enum NavegadorWeb { FIREFOX, CHROME, OPERA }
 	
 	public static WebDriver obtenerInstancia(NavegadorWeb navegador) {
 		if (navegador == NavegadorWeb.FIREFOX) {
 			return getDriverFirefox();
+		} else if (navegador == NavegadorWeb.OPERA) {
+			return getDriverOpera();
 		}
 		return getDriverChrome();
+	}
+
+	private static WebDriver getDriverOpera() {
+		WebDriverManager.operadriver().setup();
+		WebDriver driver = new OperaDriver();
+		driver.manage().window().maximize();
+		return driver;
 	}
 
 	private static WebDriver getDriverChrome() {
