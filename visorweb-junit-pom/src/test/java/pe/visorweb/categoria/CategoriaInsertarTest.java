59622a8504ec993e516f8510159fa4ef5cd9124c
Carlos Altamirano B <alt.carlos@gmail.com>
2022-06-11 19:10:28 -0500

    Se agrega test de categoria

---
+++ visorweb-junit-pom/src/test/java/pe/visorweb/categoria/CategoriaInsertarTest.java
@@ -0,0 +1,81 @@
+package pe.visorweb.categoria;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import pe.visorweb.driver.VisorWebDriver;
+import pe.visorweb.driver.VisorWebDriver.NavegadorWeb;
+import pe.visorweb.paginas.categoria.PaginaRegistrarCategoria;
+import pe.visorweb.paginas.login.LoginPagina;
+
+public class CategoriaInsertarTest {
+
+	private static WebDriver driver;
+	private static LoginPagina paginaLogin;
+	private static PaginaRegistrarCategoria paginaRegistrarCategoria;
+	
+	private String url = "http://localhost:8080/VisorWeb/index.xhtml";
+	
+	@BeforeAll
+	public static void configuracionInicial() {
+		driver = VisorWebDriver.obtenerInstancia(NavegadorWeb.CHROME);
+		paginaLogin = new LoginPagina(driver);
+		paginaRegistrarCategoria = new PaginaRegistrarCategoria(driver);
+	}
+	
+	@Test
+	public void testInsertarCategoriaExitosa() throws InterruptedException {
+		paginaLogin.cargar(url);
+		paginaLogin.iniciarSesion("admin", "clave");
+		
+		paginaRegistrarCategoria.cargar();
+		paginaRegistrarCategoria.guardar("Categoria con Page Object Model");
+		
+		String mensajeEsperado = "Se guardó de manera correcta la Categoría";
+		String mensajeObtenido = paginaRegistrarCategoria.getMensajeObtenido();
+		
+		Assertions.assertEquals(mensajeEsperado, mensajeObtenido);
+		
+	}
+	
+	@Test
+	public void testInsertarCategoriaVacia() throws InterruptedException {
+		paginaLogin.cargar(url);
+		paginaLogin.iniciarSesion("admin", "clave");
+		
+		paginaRegistrarCategoria.cargar();
+		paginaRegistrarCategoria.guardar("");
+		
+		String mensajeEsperado = "Nombre: Error de validación: se necesita un valor.";
+		String mensajeObtenido = paginaRegistrarCategoria.getMensajeObtenido();
+		
+		Assertions.assertEquals(mensajeEsperado, mensajeObtenido);
+		
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
