d0643fa1170f852239a298e0df0d22b3fe4d3b11
Carlos Altamirano B <alt.carlos@gmail.com>
2022-06-18 17:54:06 -0500

    se agrega categoria insertar steps

--- visorweb-cucumber/src/test/java/pe/visorweb/categoria/insertar/CategoriaInsertarSteps.java
+++ visorweb-cucumber/src/test/java/pe/visorweb/categoria/insertar/CategoriaInsertarSteps.java
@@ -1,35 +1,61 @@
 package pe.visorweb.categoria.insertar;
 
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pe.visorweb.driver.VisorWebDriver;
+import pe.visorweb.driver.VisorWebDriver.NavegadorWeb;
+import pe.visorweb.paginas.categoria.PaginaRegistrarCategoria;
+import pe.visorweb.paginas.login.LoginPagina;
 
 public class CategoriaInsertarSteps {
 
+	private WebDriver driver;
+	private LoginPagina paginaLogin;
+	private PaginaRegistrarCategoria paginaRegistrarCategoria;
+	
+	@Before
+	public void configuracionInicial() {
+		driver = VisorWebDriver.obtenerInstancia(NavegadorWeb.CHROME);
+		paginaLogin = new LoginPagina(driver);
+		paginaRegistrarCategoria = new PaginaRegistrarCategoria(driver);
+	}
+	
+	@After
+	public void liberarRecursos() {
+		driver.close();
+	}
+	
     @Given("cargo la página login del aplicativo Visor Web")
 	public void cargarPaginaVisorWeb() {
-        System.out.println("cargar pagina visor web...");
+        paginaLogin.cargar("http://localhost:8080/VisorWeb/index.xhtml");
     }
 
     @And("inicio sesión con el usuario {string} y la clave {string}")
     public void iniciarSesion(String usuario, String clave) {
-        System.out.println("iniciar sesion el usuario: " + usuario + " y clave: " + clave);
+        paginaLogin.iniciarSesion(usuario, clave);
     }
 
     @When("ingreso a la pagina registrar categoría")
-    public void cargarPaginaRegistrarCategoria() {
-        System.out.println("cargando pagina registrar categoría...");
+    public void cargarPaginaRegistrarCategoria() throws InterruptedException {
+        paginaRegistrarCategoria.cargar();
     }
 
     @And("escribo la categoría {string} y hago clic en el botón guardar")
-    public void registrarCategoria(String nombreCategoria) {
-        System.out.println("registrar categoria: "  + nombreCategoria);
+    public void registrarCategoria(String nombreCategoria) throws InterruptedException {
+        paginaRegistrarCategoria.guardar(nombreCategoria);
     }
 
     @Then("el aplicativo muestra el mensaje {string}")
     public void obtenerRespuesta(String mensajeEsperado) {
-        System.out.println("valor obtenido == " + mensajeEsperado);
+        assertEquals(mensajeEsperado, paginaRegistrarCategoria.getMensajeObtenido());
     }
 	
 }
